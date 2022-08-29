df7b8ad80476b2958b96b67dd851cfb4672bd6db
Hakan Bilgi <hknblg34@gmail.com>
2022-08-29 20:53:26 +0300

    array 2

--- clarusway-full-stack-tr-12-22/javascript/class-notes/js09_arrays/arrays.js
+++ clarusway-full-stack-tr-12-22/javascript/class-notes/js09_arrays/arrays.js
@@ -6,3 +6,49 @@ console.log("***** ARRAYS ******");
 
 //* Dizi Tanimlama
 //* ---------------------------------------------------------
+
+// ! 1.Yöntem (Array Literal) - Tercih edilen yöntem
+const isimler = ["ahmet", "ismet", "can", "canan", "Matheww"];
+console.log(isimler);
+console.log(typeof isimler);
+console.log(isimler.length);
+
+const bosDizi = [];
+console.log(bosDizi);
+
+// ! 2.Yöntem (Object Constructor)
+const diller = new Array("C++", "javascript", "Assembly", "Go", "Ruby");
+console.log(diller);
+
+const numbers = new Array(3, 2, 1);
+console.log(numbers);
+
+//! 10 elemanlik bos bir Array oluşturdu
+const numbers1 = new Array(10);
+console.log(numbers1);
+
+// ! 3.Yöntem (Array.of())
+const veriler = Array.of(1, 2, 3);
+console.log(veriler);
+
+const veri = Array.of("Deneme");
+console.log(veri);
+
+//* Diziden Veri Okuma-Yazma (indisleme)
+//* ---------------------------------------------------------
+//!  Diziden veri okuma
+console.log(diller[1]);
+const go = diller[3];
+console.log(go);
+
+console.log(isimler[isimler.length - 1]);
+//? at()
+console.log(isimler.at(-1));
+
+//!  Diziye veri yazma
+// const isimler = ["ahmet", "ismet", "can", "canan", "Matheww"];
+isimler[1] = "Saffet";
+console.log(isimler);
+
+//!Uncaught TypeError: Assignment to constant variable.
+isimler = ["Ali", "Veli"];

80c5f591bcfd1436e04267d7503d647011c2feba
Hakan Bilgi <hknblg34@gmail.com>
2022-08-29 22:40:04 +0300

    array 7

--- clarusway-full-stack-tr-12-22/javascript/class-notes/js09_arrays/arrays.js
+++ clarusway-full-stack-tr-12-22/javascript/class-notes/js09_arrays/arrays.js
@@ -138,3 +138,82 @@ console.log(array1);
 //? ===========================================================
 //?  DIZI ERISIM METOTLARI
 //? ===========================================================
+const sayilar1 = [3, 5, 2, "2", "uc", 2, "3", "bes", "5"];
+
+//* includes()
+//*-----------------------------------------------------------
+console.log(sayilar1.includes(5)); //? true
+console.log(sayilar1.includes("5")); //? false
+
+//* indexOf(), lastIndexOf();
+//*-----------------------------------------------------------
+//* ilk eslesen indeksi dondurur.
+
+console.log(sayilar1.indexOf(2)); //? 2
+console.log(sayilar1.lastIndexOf(2)); //? 5
+console.log(sayilar1.lastIndexOf(4)); //? -1
+
+//! Odev: prompt ile konsoldan bir sayi istenmeli (string veya number
+//! olarak) eger bu girilen sayi, dizi icerisinde bulunuyorsa indisi
+//! (string ve number olarak ayri) yazdirilmalidir. Eger bulunamadiysa
+//! Aranilan bulunamamistir yazidirilmalidir.
+//!-----------------------------------------------------------
+
+//* join()
+//*-----------------------------------------------------------
+//? join, dizinin elamanlari birlestirip string hale cevirir.
+console.log(sayilar1.join(" "));
+console.log(sayilar1.join()); // 3,5...
+console.log(sayilar1);
+
+//* toString()
+//*-----------------------------------------------------------
+//? toString fonksiyonu sadece dizinin elemanlarinin aralarina
+//? (virgul) koyarak birlestirir ve string yapar.
+console.log(sayilar1.toString());
+
+//* slice()
+//*-----------------------------------------------------------
+const arabalar1 = ["BMW", "Mercedes", "Fiat", "Anadol"];
+const yeniArabalar = arabalar1.slice(2);
+console.log(yeniArabalar, arabalar1);
+
+const yeniArabalar1 = arabalar1.slice(1, 3);
+console.log(yeniArabalar1);
+
+//* concat()
+//*-----------------------------------------------------------
+const yazilar = ["Bugun", "hava", "cok", "guzel"];
+const numbersArr = [1, 2, 5, 7];
+const combinedArr = yazilar.concat(
+  true,
+  numbersArr,
+  ["aslinda", "cok", "sicak"],
+  [["x", "y"]]
+);
+console.log(combinedArr);
+
+//* every()
+//*-----------------------------------------------------------
+
+//? Tum diziyi itere eder ve aldigi callback fonksiyonuna gore
+//? test gerceklestirir.Tum elemanlar icin test basarili ise
+//? true aksi takdirde false deger dondurur.
+
+//* some()
+//*-----------------------------------------------------------
+//? Aldigi callback fonksiyonuna gore test gerceklestirir.
+//? En az bir eleman icin bile test basarili ise true aksi
+//? takdirde false deger dondurur.
+
+//* find()
+//*-----------------------------------------------------------
+//? Aldigi callback fonksiyonuna gore test gerceklestirir.
+//? Kosulu saglayan ilk dizi elemaninin dondurur.
+//? Eger hic bir eleman kosulu saglamazsa undefined dondurur.
+
+//* findIndex()
+//*-----------------------------------------------------------
+//? Aldigi callback fonksiyonuna gore test gerceklestirir.
+//? Kosulu saglayan ilk dizi elemaninin indeksini dondurur.
+//? Eger hic bir eleman kosulu saglamazsa -1 dondurur.

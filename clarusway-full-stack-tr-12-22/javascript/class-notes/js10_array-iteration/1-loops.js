fabc95cd11e7db03c2af2c17ee0dedcc8205d284
Hakan Bilgi <hknblg34@gmail.com>
2022-08-31 21:06:21 +0300

    for in

--- clarusway-full-stack-tr-12-22/javascript/class-notes/js10_array-iteration/1-loops.js
+++ clarusway-full-stack-tr-12-22/javascript/class-notes/js10_array-iteration/1-loops.js
@@ -4,42 +4,69 @@
 
 console.log("******* LOOPS IN ARRAYS ******");
 
-
-
+//?-------------- ÖRNEK -------------------
 const dizi = [-5, 15, 22, -4, 45, 78];
 
-const topla = (n) => {
-  let negatifler = 0;
-  let pozitifler = 0;
+const negatifler = [];
+const pozitifler = [];
+
+const dizelereAyir = (n) => {
   for (let i = 0; i < n.length; i++) {
     if (n[i] < 0) {
-      negatifler += n[i];
+      negatifler.push(n[i]);
     } else {
-      pozitifler += n[i];
+      pozitifler.push(n[i]);
     }
   }
-  console.log(`Dizideki Pozitif Sayıların Toplamı: ${pozitifler}`);
-  console.log(`Dizideki Pozitif Sayıların Toplamı: ${negatifler}`);
 };
-topla(dizi);
+dizelereAyir(dizi);
 
+console.log(negatifler);
+console.log(pozitifler);
+console.log(dizi);
 
+//?-------------- ÖRNEK -------------------
+//? Dizideki notlarin ortalamasini hesaplayiniz.
+const notlar = [55, 77, 23, 89, 100];
 
+let toplam = 0;
 
+for (let i = 0; i < notlar.length; i++) {
+  toplam += notlar[i];
+}
 
-
-
+console.log("ORTALAMA:", toplam / notlar.length);
 
 //* ======================================================
 //*                   FOR-IN LOOP
 //* ======================================================
 
+//*-------------------------------------------------------
+//* SORU: Ogrenciler dizisinde ogrenci isimleri saklanmaktadir.
+//* ogrencileri aramamizi saglayacak ve aranilan ogrenciden
+//* ne kadar sayida bulunuldugunu  ana programa döndürecek bir
+//* fonksiyonu yaziniz. Eger aranilan isimde bir ogrenci yok ise
+//* fonksiyon "ogrenci bulunamadi" dondurulmelidir.
+//*--------------------------------------------------------
 
+const students = ["ahmet", "mehmet", "ismet", "ahmet", "can", "mehmet", "cem"];
 
+const findStudents = (arr, search) => {
+  let counter = 0;
+  for (let i in arr) {
+    if (search === arr[i]) {
+      counter++;
+    }
+  }
+  if (!counter) {
+    return `${search} can not be found`;
+  } else {
+    return `${search} found ${counter} times`;
+  }
+};
 
-
-
-
+const name = prompt("Please enter a name").toLowerCase();
+console.log(findStudents(students, name));
 
 //* ======================================================
 //*                   FOR-OF LOOP

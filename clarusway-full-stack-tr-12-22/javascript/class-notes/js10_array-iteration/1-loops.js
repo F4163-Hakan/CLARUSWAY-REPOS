0288378488aae0bb5a2b5807bd4c9b00f3e6112f
Hakan Bilgi <hknblg34@gmail.com>
2022-08-31 20:32:19 +0300

    array push

--- clarusway-full-stack-tr-12-22/javascript/class-notes/js10_array-iteration/1-loops.js
+++ clarusway-full-stack-tr-12-22/javascript/class-notes/js10_array-iteration/1-loops.js
@@ -6,6 +6,24 @@ console.log("******* LOOPS IN ARRAYS ******");
 
 
 
+const dizi = [-5, 15, 22, -4, 45, 78];
+
+const topla = (n) => {
+  let negatifler = 0;
+  let pozitifler = 0;
+  for (let i = 0; i < n.length; i++) {
+    if (n[i] < 0) {
+      negatifler += n[i];
+    } else {
+      pozitifler += n[i];
+    }
+  }
+  console.log(`Dizideki Pozitif Sayıların Toplamı: ${pozitifler}`);
+  console.log(`Dizideki Pozitif Sayıların Toplamı: ${negatifler}`);
+};
+topla(dizi);
+
+
 
 
 

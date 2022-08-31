fea0f862f3eb16df4216f11ca307112936506b53
Hakan Bilgi <hknblg34@gmail.com>
2022-08-31 22:11:23 +0300

    for each

--- clarusway-full-stack-tr-12-22/javascript/class-notes/js10_array-iteration/1-loops.js
+++ clarusway-full-stack-tr-12-22/javascript/class-notes/js10_array-iteration/1-loops.js
@@ -76,22 +76,23 @@ const students = ["ahmet", "mehmet", "ismet", "ahmet", "can", "mehmet", "cem"];
 //* kapsayacak sekilde guncellenmis halidir. Bu dongude dizi
 //* icersindeki veriye erisirken indisleme kullanmaya gerek yoktur.
 
-
-const finstudents = ["ahmet", "mehmet", "ismet", "ahmet", "can", "mehmet", "cem"];
-
-const findStudents = (arr, search) => {
+const findStudentsOf = (arr, search) => {
   let counter = 0;
-  for (let i in arr) {
-    if (search === arr[i]) {
-      counter++;
-    }
+  for (let item of arr) {
+    //? Ternary
+    // search === item ? counter++ : null;
+
+    //! Short-circuit yöntemi: && => kosul dogru (true) ise ifadeyi calisitir.
+    search === item && counter++;
+
+    //! Short-circuit yöntemi: || => kosul yanlis (false) ise ifadeyi çalıştır.
+    search === item || counter++;
   }
-  if (!counter) {
-    return `${search} can not be found`;
-  } else {
-    return `${search} found ${counter} times`;
-  }
+
+  return !counter
+    ? `${search} can not be found`
+    : `${search} found ${counter} times`;
 };
 
-const name = prompt("Please enter a name").toLowerCase();
-console.log(findStudents(students, name));
+const studentName = prompt("Please enter a name").toLowerCase();
+console.log(findStudentsOf(students, studentName));

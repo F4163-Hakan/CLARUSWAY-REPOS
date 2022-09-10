f8ae2484adfbf9d0628370851e166e54e497c0fb
Hakan Bilgi <hknblg34@gmail.com>
2022-09-10 12:44:21 +0300

    html collection

--- clarusway-full-stack-tr-12-22/javascript/class-notes/js13_dom-intro/1-selectors.js
+++ clarusway-full-stack-tr-12-22/javascript/class-notes/js13_dom-intro/1-selectors.js
@@ -26,19 +26,50 @@ button.style.padding = "1rem";
 button.style.fontSize = "1.1rem";
 myInput.style.padding = "1rem";
 
+//*===========================================
+//*          GETELEMENTSBYTAGNAME()
+//*===========================================
 
-
-//* EXAMPLE-3
+//*EXAMPLE-3
 //*-------------------------------------------
 
 const list = document.getElementsByTagName("li");
-console.log(list);
-list[2].style.color="red";
-list.item(3).style.color="blue";
+console.log(list); //? HTML Collection
+list[2].style.color = "red";
 const elementThree = list.item(3);
-elementThree.style.color="blue";
+elementThree.style.color = "blue";
 elementThree.textContent = "React / Vue / Angular";
 
-list[4].innerText = "Django /Flask";
+list[4].innerText = "Django / Flask";
 
+console.log(list[1].textContent);
+console.log(list[1].innerText);
 
+//? HTML kodlarini calistirir. (Guvenlik acisindan problemli)
+list[4].innerHTML = "<a href='https://www.clarusway.com'>Clarusway Web</a>";
+
+//*===========================================
+//*          GETELEMENTSBYCLASSNAME()
+//*===========================================
+
+//* EXAMPLE-4
+//* ------------------------------------------
+const myList = document.getElementsByClassName("list");
+console.log(myList); //? HTML Collection
+console.log(myList[0].innerText);
+
+//? HRML Collection'larda Dizi metotlari dogrudan kullanilamaz.
+//? Spread ve Array.from() ile kullanilabilri hale gelir.
+
+//?Array.from()
+const myListArray = Array.from(myList);
+myListArray.forEach((item) => console.log(item.innerText));
+
+//?Spread
+[...myList].forEach((item) => console.log(item.innerText));
+
+[...myList].forEach((item) => (item.style.color = "red"));
+
+//* ========================================
+//*              QUERYSELECTOR()
+//* ========================================

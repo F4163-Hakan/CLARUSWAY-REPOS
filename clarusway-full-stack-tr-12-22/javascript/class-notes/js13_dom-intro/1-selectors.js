27857deec8397c4383b66b263858f3883141a607
Hakan Bilgi <hknblg34@gmail.com>
2022-09-10 12:42:32 +0300

    dom4

--- clarusway-full-stack-tr-12-22/javascript/class-notes/js13_dom-intro/1-selectors.js
+++ clarusway-full-stack-tr-12-22/javascript/class-notes/js13_dom-intro/1-selectors.js
@@ -25,3 +25,20 @@ button.style.borderRadius = "10px";
 button.style.padding = "1rem";
 button.style.fontSize = "1.1rem";
 myInput.style.padding = "1rem";
+
+
+
+//* EXAMPLE-3
+//*-------------------------------------------
+
+const list = document.getElementsByTagName("li");
+console.log(list);
+list[2].style.color="red";
+list.item(3).style.color="blue";
+const elementThree = list.item(3);
+elementThree.style.color="blue";
+elementThree.textContent = "React / Vue / Angular";
+
+list[4].innerText = "Django /Flask";
+
+

f0a114f8993defee7ae6c829e64d854c6fbbb7a5
Hakan Bilgi <hknblg34@gmail.com>
2022-09-12 21:00:25 +0300

    add

--- clarusway-full-stack-tr-12-22/javascript/class-notes/js14_events/1-events-intro/events.js
+++ clarusway-full-stack-tr-12-22/javascript/class-notes/js14_events/1-events-intro/events.js
@@ -7,6 +7,15 @@ console.log("****** EVENTS *******");
 //* EXAMPLE-1 (Mouse Over, Mouse Out )
 //* -------------------------------------------------------
 
+const h1 = document.querySelector("#header h1");
+h1.onmouseover = function(){
+  h1.style.color = "red";
+  
+};
+h1.onmouseout = function(){
+  h1.style.color = "black";
+}
+
 
 
 

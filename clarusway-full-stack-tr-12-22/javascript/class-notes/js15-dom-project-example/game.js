8205cc0888d3a6772aad7c2dcc413512e4514270
Hakan Bilgi <hknblg34@gmail.com>
2022-09-13 21:52:45 +0300

    vs

--- clarusway-full-stack-tr-12-22/javascript/class-notes/js15-dom-project-example/game.js
+++ clarusway-full-stack-tr-12-22/javascript/class-notes/js15-dom-project-example/game.js
@@ -12,7 +12,6 @@ let score = 10;
 let topScore = 0;
 
 //* CheckBtn basildiginda kontrolleri yap
-
 document.querySelector(".check-btn").addEventListener("click", () => {
   const guessInput = Number(document.querySelector(".guess-input").value);
   const msg = document.querySelector(".msg");
@@ -25,18 +24,42 @@ document.querySelector(".check-btn").addEventListener("click", () => {
   } else if (randomNumber === guessInput) {
     msg.innerHTML = `Congrats You Win <i class="fa-solid fa-face-grin-hearts fa-2x"></i> `;
     body.className = "bg-success";
+    document.querySelector(".check-btn").disabled = true;
     if (score > topScore) {
       topScore = score;
       document.querySelector(".top-score").textContent = topScore;
     }
     document.querySelector(".secret-number").textContent = randomNumber;
+
+    //! eger rasgele!= input.value
   } else {
     score--;
+    if (score > 0) {
+      guessInput > randomNumber
+        ? (msg.innerHTML = `<i class="fa-solid fa-arrow-trend-down fa-2x"></i> DECREASE `)
+        : (msg.innerHTML = `<i class="fa-solid fa-arrow-trend-up fa-2x"></i> INCREASE `);
+    } else {
+      msg.innerHTML = `You Lost <i class="fa-regular fa-face-sad-tear fa-2x"></i>`;
+      document.querySelector(".secret-number").textContent = randomNumber;
+      body.className = "bg-danger";
+      document.querySelector(".check-btn").disabled = true;
+    }
 
-    guessInput > randomNumber
-      ? (msg.innerText = "DECREASE")
-      : (msg.innerText = "INCREASE");
+    document.querySelector(".score").textContent = score;
   }
+});
+
+//* again basildiginda oyunu baslangic dgerlerin kur
+document.querySelector(".again-btn").addEventListener("click", () => {
+  score = 10;
+  document.querySelector(".score").textContent = score;
+  const randomNumber = Math.round(Math.random() * 100);
+  document.querySelector(".secret-number").textContent = "?";
+  console.log(randomNumber);
+  document.querySelector(".check-btn").disabled = false;
+  document.querySelector("body").classList.remove("bg-success", "bg-danger");
+  document.querySelector(".guess-input").value = "";
+  document.querySelector(".msg").innerText = `Starting..`;
 });
 
 //? eger score > topScore

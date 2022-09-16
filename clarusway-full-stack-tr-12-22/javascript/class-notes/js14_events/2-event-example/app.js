4a2fd2247cf53f903a6fd88be98626988c4f1bd5
Hakan Bilgi <hknblg34@gmail.com>
2022-09-16 16:25:21 +0300

    focus

--- clarusway-full-stack-tr-12-22/javascript/class-notes/js14_events/2-event-example/app.js
+++ clarusway-full-stack-tr-12-22/javascript/class-notes/js14_events/2-event-example/app.js
@@ -1,24 +1,28 @@
-console.log("**** app.js *******");
+//? onload event handler
+window.addEventListener("load", () => {
+  langInput.focus();
+});
 
-//? Selectors
 const langInput = document.querySelector(".lang-input");
 const addBtn = document.querySelector("#add-btn");
 const deleteBtn = document.querySelector("#delete-btn");
-const langList = document.getElementById("lang-list");
+const langList = document.querySelector("#lang-list");
 
 const newUl = document.createElement("ul");
 langList.appendChild(newUl);
 
-//? addBtn event handler
-addBtn.addEventListener("click", () => {
-  if (!langInput.value) {
+
+addBtn.addEventListener("click", ()=>{
+  if(!langInput.value){
     alert("Please enter a language");
-  } else {
-    newUl.innerHTML += `<li>${langInput.value}</li>`;
-    langInput.value = "";
+    else{
+      newUl.innerHTML+= `<li>${langInput.value}</li>`
+      langInput.value = "";
+    }
   }
   langInput.focus();
 });
+
 
 //? deleteBtn event handler
 deleteBtn.addEventListener("click", () => {
@@ -47,7 +51,4 @@ langInput.addEventListener("keydown", (event) => {
   }
 });
 
-//? onload event handler
-window.addEventListener("load", () => {
-  langInput.focus();
-});
+

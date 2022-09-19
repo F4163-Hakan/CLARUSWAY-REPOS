c413ef9715a728408e4a708d57a1af7f7130d73a
Hakan Bilgi <hknblg34@gmail.com>
2022-09-19 21:24:12 +0300

    create

--- clarusway-full-stack-tr-12-22/javascript/class-notes/js16_todo_solution/app.js
+++ clarusway-full-stack-tr-12-22/javascript/class-notes/js16_todo_solution/app.js
@@ -1,3 +1,42 @@
 //* ======================================================
 //*                     TODO APP
 //* ======================================================
+
+//?Selectors
+const addBtn = document.getElementById("todo-button");
+const todoInput = document.getElementById("todo-input");
+const todoUl = document.getElementById("todo-ul");
+
+addBtn.addEventListener("click", () => {
+  if (todoInput.value.trim() === "") {
+    alert("PLease enter new todo");
+  } else {
+    const newTodo = {
+      id: new Date().getTime(),
+      completed: false,
+      text: todoInput.value,
+    };
+
+    createListElement(newTodo);
+  }
+});
+
+const createListElement = (newTodo) => {
+  //? yeni bir li elementi olustur ve bu elemente obje icerisindeki
+  //? id degerini ve completed class'ini ata
+  const li = document.createElement("li");
+  // li.id = newTodo.id;
+  li.setAttribute("id", newTodo.id);
+
+  //? okey ikonu olustur ve li elementine bagla
+  const okIcon = document.createElement("i");
+  okIcon.setAttribute("class", "fas fa-check");
+  li.appendChild(okIcon);
+
+  //? todo basligi icin bir p elementi ve yazi dugumu olusturarak li'ye bagla
+
+  //? delete ikonu olustur ve li elementine bagla
+  console.log(li);
+
+  todoUl.appendChild(li);
+};

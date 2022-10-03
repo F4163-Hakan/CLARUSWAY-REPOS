1f22ea55d211dc754fdb4acc84406eaf5b1520f2
Hakan Bilgi <hknblg34@gmail.com>
2022-10-03 22:10:29 +0300

    test run descript

--- clarusway-full-stack-tr-12-22/javascript/class-notes/js23-testing/src/math.test.js
+++ clarusway-full-stack-tr-12-22/javascript/class-notes/js23-testing/src/math.test.js
@@ -44,3 +44,28 @@ describe("fibonacci", () => {
     expect(fibonacci).toBeDefined();
   });
 });
+
+
+// //? içindeki isim açıklama için yazılıyor, aslında önemsiz
+// //? fakat test edilecek şeyi yazmak best practise kullanım ismini yazmak
+
+// describe("calculateTotal", () => {
+//   //* test caselerini burada yazmaya başliyoruz.Günlük konuşma diline yakın
+//   //* çıktıda daha okunabilir olması için, yoksa ARMUT yazsak da olur.
+
+//   //? 1- test edilen fonksiyon silinmiş/değişmiş olmasın
+//   it('sholud exist', ()=> {
+//     expect(calculateTotal).toBeDefined();
+//   })
+
+//   //? 2- fonksiyon 100 lirayı %20 faizle 120 olarak döndürmeli
+//   it('should return 0.2 tax', () =>{
+//     expect(calculateTotal(100, 0.2)).toBe(120)
+//   })
+
+//   //? 3- fonksiyon 200 lirayı default faizle (%18) 236 olarak döndürmeli
+//   it('should return default tax', () =>{
+//     expect(calculateTotal(200)).toBe(236)
+//   })
+
+// });

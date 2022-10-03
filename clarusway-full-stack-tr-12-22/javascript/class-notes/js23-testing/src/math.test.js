eea75dd7e62db3b3503af189dcb40a8f6cc68536
Hakan Bilgi <hknblg34@gmail.com>
2022-10-03 22:42:11 +0300

    test finish

--- clarusway-full-stack-tr-12-22/javascript/class-notes/js23-testing/src/math.test.js
+++ clarusway-full-stack-tr-12-22/javascript/class-notes/js23-testing/src/math.test.js
@@ -31,11 +31,23 @@ describe("celsiusToFahrenheit", () => {
   it("should exist", () => {
     expect(celsiusToFahrenheit).toBeDefined();
   });
+
+  it("should return a number", () => {
+    expect(celsiusToFahrenheit(10)).toBe(50);
+  });
+
+  it("should convert 20 C to 68 F", () => {
+    expect(celsiusToFahrenheit(20)).toBe(68);
+  });
 });
 
 describe("fahrenheitToCelsius", () => {
   it("should exist", () => {
     expect(fahrenheitToCelsius).toBeDefined();
+  });
+
+  it("should convert 32F to 0C", () => {
+    expect(fahrenheitToCelsius(32)).toBe(0);
   });
 });
 
@@ -43,29 +55,22 @@ describe("fibonacci", () => {
   it("should exist", () => {
     expect(fibonacci).toBeDefined();
   });
+
+  it("should return a number", () => {
+    expect(fibonacci()).toBe(1);
+  });
+
+  it("should return 1 for 1", () => {
+    expect(fibonacci(1)).toBe(1);
+  });
+
+  it("should return 55 for 10th index", () => {
+    expect(fibonacci(10)).toBe(55);
+  });
+
+  it("should throw error if number is less than 1", () => {
+    expect(() => fibonacci(0)).toThrow(
+      new Error("Must enter a number greater than 0")
+    );
+  });
 });
-
-
-// //? içindeki isim açıklama için yazılıyor, aslında önemsiz
-// //? fakat test edilecek şeyi yazmak best practise kullanım ismini yazmak
-
-// describe("calculateTotal", () => {
-//   //* test caselerini burada yazmaya başliyoruz.Günlük konuşma diline yakın
-//   //* çıktıda daha okunabilir olması için, yoksa ARMUT yazsak da olur.
-
-//   //? 1- test edilen fonksiyon silinmiş/değişmiş olmasın
-//   it('sholud exist', ()=> {
-//     expect(calculateTotal).toBeDefined();
-//   })
-
-//   //? 2- fonksiyon 100 lirayı %20 faizle 120 olarak döndürmeli
-//   it('should return 0.2 tax', () =>{
-//     expect(calculateTotal(100, 0.2)).toBe(120)
-//   })
-
-//   //? 3- fonksiyon 200 lirayı default faizle (%18) 236 olarak döndürmeli
-//   it('should return default tax', () =>{
-//     expect(calculateTotal(200)).toBe(236)
-//   })
-
-// });

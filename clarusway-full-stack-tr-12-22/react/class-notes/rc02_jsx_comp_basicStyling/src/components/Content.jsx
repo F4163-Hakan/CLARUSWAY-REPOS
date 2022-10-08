9defbf3adb96b5de1f4fe074c48bae3073896a13
Hakan Bilgi <hknblg34@gmail.com>
2022-10-08 12:50:43 +0300

    css

--- clarusway-full-stack-tr-12-22/react/class-notes/rc02_jsx_comp_basicStyling/src/components/Content.jsx
+++ clarusway-full-stack-tr-12-22/react/class-notes/rc02_jsx_comp_basicStyling/src/components/Content.jsx
@@ -9,4 +9,24 @@
 //* class isimleri icin className kullanilir.
 //* Material UI, Styled Component, Sass, Bootstrap v.b harici kutuphaneler kullanilabilir.
 
+//?Harici stillneidmre dosyasi
+
 // ? Dahili bir resim eklemek icin import yapmak gereklidir.
+
+const Content = () => {
+  return (
+    <div>
+      <h2>React JS</h2>
+      <p>
+        React, Kullanici arabirimi tasarlamak icin kullanilan bir JS
+        kütüphanesidir.
+      </p>
+      <img
+        src="https://cdn.pixabay.com/photo/2017/02/15/13/40/tulips-2068692__340.jpg"
+        alt="img1"
+      />
+    </div>
+  );
+};
+
+export default Content;

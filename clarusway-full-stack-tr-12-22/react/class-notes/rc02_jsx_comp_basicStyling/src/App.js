58f3c0e8b3b89443348d11f7be7d1905d20f076a
Hakan Bilgi <hknblg34@gmail.com>
2022-10-08 12:30:33 +0300

    components

--- clarusway-full-stack-tr-12-22/react/class-notes/rc02_jsx_comp_basicStyling/src/App.js
+++ clarusway-full-stack-tr-12-22/react/class-notes/rc02_jsx_comp_basicStyling/src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
 
+//? React,JSX kullanmaktadir.
+//? JSX'de, HTMl elementleri dogrudan JS icerisinde kullanilabilir
+
+import Header from "./components/Header";
+
+//! Componentler, HTML,CSS ve JS kodlarindan meydana gelmiş
+//! bir kod parcacigidir.
+
+//! Bir componentin return () kismi ise JSX kodlari icerir.
+
+//! JSX'in kendine gore bazi kurallari vardir. Ornegin bir
+//! component eger birden fazla html elementi dondurmesi
+//! gerekirse bunlari tek bir container elemani ile sarmayalarak
+//! dondurmelidir.container icin div, article, header, <> v.b
+//! herhangi bir element kullanilabilir.
+
+//? App componentinin function declaration ile tanimlanmasi
 function App() {
-  return <h1>JSX & COMPONENTS & BASİC STYLING</h1>;
+  return <Header />;
 }
 
 export default App;

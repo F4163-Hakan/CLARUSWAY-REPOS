c0d87f6e576d80c47984cae86420dd4ee01d48d2
Hakan Bilgi <hknblg34@gmail.com>
2022-10-12 21:34:31 +0300

    class component

--- clarusway-full-stack-tr-12-22/react/class-notes/rc05_events_useStateHook/src/components/2-classComponent/Counter.jsx
+++ clarusway-full-stack-tr-12-22/react/class-notes/rc05_events_useStateHook/src/components/2-classComponent/Counter.jsx
@@ -1,4 +1,4 @@
-//* ============================ CLASS COMPONENTS AND STATE ============================
+//* ================= CLASS COMPONENTS AND STATE ====================
 //* React'da Class-Component'ler ES6 class yapisina dayanmaktadir.
 //* Cok fazla boilerplate kod icermektedir.
 //* Ancak Class-Component'ler React'da state'leri barindiran ilk component yapisidir.
@@ -7,10 +7,33 @@
 //* State her degistiginde React bu componenti yeninden render eder.
 //* Bir state'e baslangıc degeri constructor metodu icersinde this.state ile atanabilir
 //* constructor'in disinda state, setState() metodu ile degistilebilir.
-//* ====================================================================================
+//* ====================================================================
 
-const Counter = () => {
-  return <div>Counter</div>;
-};
+import { Component } from "react";
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+    //! count state'ine baslangic degeri atadik
+    this.state = {
+      count: 0,
+    };
+  }
+
+  render() {
+    return (
+      <div className="container text-center mt-4">
+        <h1>CLASSFUL COMPONENTS</h1>
+        <h2 className="display-4 text-danger">COUNT:</h2>
+        <button className="btn btn-success">INC</button>
+
+        <button className="btn btn-dark">CLR</button>
+
+        <button className="btn btn-danger">DEC</button>
+      </div>
+    );
+  }
+}
 
 export default Counter;

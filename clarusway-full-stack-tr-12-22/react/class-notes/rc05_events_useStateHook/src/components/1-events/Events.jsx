f491abdbc318eea4a202156a08d8d705f2a6da22
Hakan Bilgi <hknblg34@gmail.com>
2022-10-12 20:46:36 +0300

    onClick

--- clarusway-full-stack-tr-12-22/react/class-notes/rc05_events_useStateHook/src/components/1-events/Events.jsx
+++ clarusway-full-stack-tr-12-22/react/class-notes/rc05_events_useStateHook/src/components/1-events/Events.jsx
@@ -12,9 +12,35 @@
 //? Ayrinti icin : https://reactjs.org/docs/events.html
 
 const Events = () => {
+  const handleClick = () => {
+    alert("Btn Clicked");
+  };
+  const handleClear = (msg) => {
+    alert(msg);
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
   return (
-    <div>
-      <button>Click</button>
+    <div className="container text-center mt-4">
+      <button onClick={handleClick} className="btn btn-success">
+        Click
+      </button>
+
+      <button
+        onClick={() => handleClear("Clear Btn Clicked")}
+        className="btn btn-dark"
+      >
+        Clear
+      </button>
+      <button onClick={handleChange} className="btn btn-danger">
+        Change
+      </button>
+      {/* <button onClick={(e) => handleChange(e)} className="btn btn-danger">
+        Change
+      </button> */}
     </div>
   );
 };

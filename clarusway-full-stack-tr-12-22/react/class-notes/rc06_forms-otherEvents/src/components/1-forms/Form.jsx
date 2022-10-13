7ed12a41be642c3d4cc4f0a1f21287a7d2df3be8
Hakan Bilgi <hknblg34@gmail.com>
2022-10-13 20:16:04 +0300

    form bootstrap

--- clarusway-full-stack-tr-12-22/react/class-notes/rc06_forms-otherEvents/src/components/1-forms/Form.jsx
+++ clarusway-full-stack-tr-12-22/react/class-notes/rc06_forms-otherEvents/src/components/1-forms/Form.jsx
@@ -2,40 +2,26 @@ const Form = () => {
   return (
     <form>
       <h1 className="display-5 text-danger">FORMS</h1>
+
       <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">
-          Email address
+        <label htmlFor="username" className="form-label">
+          Username
         </label>
-        <input
-          type="email"
-          className="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-        />
-        <div id="emailHelp" className="form-text">
-          We'll never share your email with anyone else.
-        </div>
+        <input type="text" className="form-control" id="username" />
       </div>
       <div className="mb-3">
-        <label htmlFor="exampleInputPassword1" className="form-label">
+        <label htmlFor="email" className="form-label">
+          Email
+        </label>
+        <input type="email" className="form-control" id="email" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="password" className="form-label">
           Password
         </label>
-        <input
-          type="password"
-          className="form-control"
-          id="exampleInputPassword1"
-        />
+        <input type="password" className="form-control" id="password" />
       </div>
-      <div className="mb-3 form-check">
-        <input
-          type="checkbox"
-          className="form-check-input"
-          id="exampleCheck1"
-        />
-        <label className="form-check-label" htmlFor="exampleCheck1">
-          Check me out
-        </label>
-      </div>
+
       <button type="submit" className="btn btn-primary">
         Submit
       </button>

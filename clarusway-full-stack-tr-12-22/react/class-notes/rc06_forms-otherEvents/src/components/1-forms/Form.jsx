8ea032905703e341a6f4caf49ed98e78ecaa376d
Hakan Bilgi <hknblg34@gmail.com>
2022-10-13 20:37:17 +0300

    useState

--- clarusway-full-stack-tr-12-22/react/class-notes/rc06_forms-otherEvents/src/components/1-forms/Form.jsx
+++ clarusway-full-stack-tr-12-22/react/class-notes/rc06_forms-otherEvents/src/components/1-forms/Form.jsx
@@ -1,13 +1,38 @@
+import { useState } from "react";
+
 const Form = () => {
+  const [username, setUsername] = useState("Mehmet");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    console.log("Submitted");
+    alert(`username: ${username}
+          email:,
+          password:
+    `);
+  };
+
+  const handleUsername = (e) => {
+    console.log(e.target.value);
+    setUsername(e.target.value);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h1 className="display-5 text-danger">FORMS</h1>
 
       <div className="mb-3">
         <label htmlFor="username" className="form-label">
-          Username
+          Username: <span>{username}</span>
         </label>
-        <input type="text" className="form-control" id="username" />
+        <input
+          type="text"
+          className="form-control"
+          id="username"
+          value={username}
+          onChange={handleUsername}
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="email" className="form-label">

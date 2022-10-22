a62329887c39b11f632c0f36b68b9e21c0bc2500
Hakan Bilgi <hknblg34@gmail.com>
2022-10-22 13:49:30 +0300

    useLocation

---
+++ clarusway-full-stack-tr-12-22/react/class-notes/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
@@ -0,0 +1,25 @@
+import { useParams, useLocation } from "react-router-dom";
+
+const PersonDetail = () => {
+  const { id } = useParams();
+  console.log(id);
+
+  const { state: person } = useLocation();
+  console.log(person);
+
+  return (
+    <div className="container text-center">
+      <h3>
+        {person.first_name} {person.last_name}
+      </h3>
+      <img className="rounded" src={person.avatar} alt="" />
+      <p>{person.email}</p>
+      <div>
+        <button className="btn btn-success me-2">Home</button>
+        <button className="btn btn-warning">Go Back</button>
+      </div>
+    </div>
+  );
+};
+
+export default PersonDetail;

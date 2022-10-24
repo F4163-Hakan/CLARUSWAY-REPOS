f5bd1d5d2f6fe0d4e39d19e4b96f9b2444f650e2
Hakan Bilgi <hknblg34@gmail.com>
2022-10-24 22:50:22 +0300

    nested routing

--- clarusway-full-stack-tr-12-22/react/class-notes/rc11-12_router-example/rc11-12_router-example-inClass/src/App.js
+++ clarusway-full-stack-tr-12-22/react/class-notes/rc11-12_router-example/rc11-12_router-example-inClass/src/App.js
@@ -7,6 +7,8 @@ import NotFound from "./pages/NotFound";
 import { Routes, Route, Link, Navigate } from "react-router-dom";
 import Paths from "./pages/Paths";
 import PersonDetail from "./pages/PersonDetail";
+import FullStack from "./pages/FullStack";
+import Aws from "./pages/Aws";
 
 //? Link, NavLink ve Navigate componentleri declerative routing
 //? gerceklestirmek icin kullanilir.
@@ -30,12 +32,19 @@ function App() {
         <Route index element={<Home />} />
         <Route path="people" element={<People />} />
         <Route path="people/:id" element={<PersonDetail />} />
-        <Route path="paths" element={<Paths />} />
+
+        {/* nested route */}
+        <Route path="paths" element={<Paths />}>
+          <Route index element={<FullStack />} />
+          {/* <Route path="fullstack" element={<FullStack />} /> */}
+          <Route path="aws" element={<Aws />} />
+        </Route>
+
         <Route path="contact" element={<Contact />} />
         {/* <Route path="*" element={<NotFound />} /> */}
 
         {/* Redirect islemleri icin Navigate componenti kullanilabilir */}
-        <Route path="*" element={<Navigate to="/" />} />
+        {/* <Route path="*" element={<Navigate to="/" />} /> */}
       </Routes>
       <Footer />
     </>

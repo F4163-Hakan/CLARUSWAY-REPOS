d880f54d3b4ded257927b8e1c6ca9071b3f2fcd0
Hakan Bilgi <hknblg34@gmail.com>
2022-10-25 21:07:45 +0300

    styled container

--- clarusway-full-stack-tr-12-22/react/class-notes/rc13_styled-comp-intro/styled-intro-inClass/src/components/Button.styled.jsx
+++ clarusway-full-stack-tr-12-22/react/class-notes/rc13_styled-comp-intro/styled-intro-inClass/src/components/Button.styled.jsx
@@ -8,6 +8,7 @@ export const Button = styled.button`
   color: ${({ primary }) => (primary ? "purple" : "white")};
 
   cursor: pointer;
+  width: 7rem;
   padding: 1rem 2rem;
   border-radius: 0.5rem;
   border: 1px solid purple;

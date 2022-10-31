5da2f9eebe1075be819841497e42af52ca009f71
Hakan Bilgi <hknblg34@gmail.com>
2022-10-31 20:36:18 +0300

    add

---
+++ clarusway-full-stack-tr-12-22/react/class-notes/rc14-recipe-app/recipe-app-solution/src/router/AppRouter.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./../components/nav/Navbar";
+import {GlobalStyles} from "../components/globalStyles/GlobalStyles"
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+    <GlobalStyles/>
+      <Navbar />
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;

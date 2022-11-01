339c229552d660de1b4cdf50110ecec18c824444
Hakan Bilgi <hknblg34@gmail.com>
2022-11-01 21:18:18 +0300

    ...

--- clarusway-full-stack-tr-12-22/react/class-notes/rc14-recipe-app/recipe-app-solution/src/components/header/Header.jsx
+++ clarusway-full-stack-tr-12-22/react/class-notes/rc14-recipe-app/recipe-app-solution/src/components/header/Header.jsx
@@ -6,9 +6,11 @@ import {
   FormContainer,
   HeaderContainer,
   MainHeader,
+  Select,
 } from "./Header.style";
 
-const Header = ({ setQuery }) => {
+const Header = ({ setQuery, setSelectedMeal, mealType }) => {
+  console.log(mealType);
   return (
     <HeaderContainer>
       <MainHeader>Recipe App</MainHeader>
@@ -21,8 +23,20 @@ const Header = ({ setQuery }) => {
         />
 
         <Button type="submit">SEARCH</Button>
-      
-      
+
+        <Select
+          name="mealType"
+          id="mealType"
+          onChange={(e) => setSelectedMeal(e.target.value)}
+        >
+          {mealType.map((meal, index) => {
+            return (
+              <option key={index} value={meal}>
+                {meal}
+              </option>
+            );
+          })}
+        </Select>
       </FormContainer>
     </HeaderContainer>
   );

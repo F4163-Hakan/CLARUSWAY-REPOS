429ec5520d3b4d764b7c4cb0a18e8c5cac4ecd03
Hakan Bilgi <hknblg34@gmail.com>
2022-11-03 21:29:21 +0300

    textfield

--- clarusway-full-stack-tr-12-22/react/class-notes/rc17_material-intro/material-intro-inClass/src/components/TypoButtons.jsx
+++ clarusway-full-stack-tr-12-22/react/class-notes/rc17_material-intro/material-intro-inClass/src/components/TypoButtons.jsx
@@ -4,11 +4,9 @@ import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import DeleteIcon from "@mui/icons-material/Delete";
 import SendIcon from "@mui/icons-material/Send";
-
-
 const TypoButtons = () => {
   return (
-     <>
+    <>
       <Container maxWidth="md">
         <Typography
           variant="h4"

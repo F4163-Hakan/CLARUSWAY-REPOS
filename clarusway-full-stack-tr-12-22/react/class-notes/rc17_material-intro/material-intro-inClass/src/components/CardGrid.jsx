1a5ce4ae8703e639078f13537fe805f18ba32c42
Hakan Bilgi <hknblg34@gmail.com>
2022-11-03 23:01:24 +0300

    ...

--- clarusway-full-stack-tr-12-22/react/class-notes/rc17_material-intro/material-intro-inClass/src/components/CardGrid.jsx
+++ clarusway-full-stack-tr-12-22/react/class-notes/rc17_material-intro/material-intro-inClass/src/components/CardGrid.jsx
@@ -29,6 +29,7 @@ export default function CardGrid() {
                       {text}
                     </Typography>
                   </CardContent>
+                  
                 </CardActionArea>
                 <CardActions>
                   <Button size="small" color="primary">

e680aa0314668551d5dc067ac5914e61e58e3328
Hakan Bilgi <hknblg34@gmail.com>
2022-11-07 22:30:29 +0300

    register son

--- clarusway-full-stack-tr-12-22/react/class-notes/rc18-19_firebase-movie-app/firebase-movie-app-solution/src/auth/firebase.js
+++ clarusway-full-stack-tr-12-22/react/class-notes/rc18-19_firebase-movie-app/firebase-movie-app-solution/src/auth/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 
 //* Your web app's Firebase configuration
 // TODO: Replace the following with your app's Firebase project configuration
@@ -19,3 +19,17 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
+
+export const createUser = async (email, password) => {
+  //? yeni bir kullanıcı oluşturmak için kullanılan firebase metodu
+  try {
+    let userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    console.log(userCredential);
+  } catch (error) {
+    console.log(error.message);
+  }
+};

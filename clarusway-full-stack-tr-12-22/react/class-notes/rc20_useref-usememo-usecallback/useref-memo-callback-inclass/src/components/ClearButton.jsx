c4548597ae22a5d84afbe4239596bde2627c863b
Hakan Bilgi <hknblg34@gmail.com>
2022-11-09 22:04:47 +0300

    ...

--- clarusway-full-stack-tr-12-22/react/class-notes/rc20_useref-usememo-usecallback/useref-memo-callback-inclass/src/components/ClearButton.jsx
+++ clarusway-full-stack-tr-12-22/react/class-notes/rc20_useref-usememo-usecallback/useref-memo-callback-inclass/src/components/ClearButton.jsx
@@ -4,6 +4,7 @@ const ClearButton = memo(({handleClear}) => {
     console.log("Render => ClearButton componenti")
     return (
         <div>
+        
             <button className='btn btn-warning' onClick={handleClear} >ClearButton</button>
         </div>
     )

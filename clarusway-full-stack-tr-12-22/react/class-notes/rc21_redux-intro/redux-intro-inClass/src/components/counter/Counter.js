e3562a64d8f1ec923d204cecb57c401bf566d87d
Hakan Bilgi <hknblg34@gmail.com>
2022-11-10 22:36:37 +0300

    todoReducer

--- clarusway-full-stack-tr-12-22/react/class-notes/rc21_redux-intro/redux-intro-inClass/src/components/counter/Counter.js
+++ clarusway-full-stack-tr-12-22/react/class-notes/rc21_redux-intro/redux-intro-inClass/src/components/counter/Counter.js
@@ -1,10 +1,11 @@
 import "./Counter.css";
 import { useDispatch, useSelector } from "react-redux";
-import { increment } from "../../redux/actions/counterAction";
+import { clear, decrement, increment } from "../../redux/actions/counterAction";
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector((state) => state.counter);
+  // const counter = useSelector((state) => state.counter);
+  const { counter } = useSelector((state) => state.count);
   return (
     <div className="app">
       <h2 className="counter-header">Counter With Redux</h2>
@@ -12,19 +13,22 @@ const Counter = () => {
       <div>
         <button
           className="counter-button positive"
-          onClick={() => dispatch(increment(username))}
+          // onClick={() => dispatch({ type: "INCREMENT" })}
+          onClick={() => dispatch(increment())}
         >
           increase
         </button>
         <button
           className="counter-button zero"
-          onClick={() => dispatch({ type: "CLEAR" })}
+          // onClick={() => dispatch({ type: "CLEAR" })}
+          onClick={() => dispatch(clear())}
         >
           reset
         </button>
         <button
           className="counter-button negative"
-          onClick={() => dispatch({ type: "DECREMENT" })}
+          // onClick={() => dispatch({ type: "DECREMENT" })}
+          onClick={() => dispatch(decrement())}
         >
           decrease
         </button>
--- clarusway-full-stack-tr-12-22/react/class-notes/rc21_redux-intro/redux-intro-inClass/src/redux/index.jsx
+++ clarusway-full-stack-tr-12-22/react/class-notes/rc21_redux-intro/redux-intro-inClass/src/redux/index.jsx
@@ -15,3 +15,14 @@
 //   }
 // };
 // export default reducer;
+
+import { legacy_createStore as createStore, combineReducers } from "redux";
+import counterReducer from "./reducers/counterReducer";
+import todoReducer from "./reducers/todoReducer";
+
+const rootReducer = combineReducers({
+  count: counterReducer,
+  todo: todoReducer,
+});
+
+export const store = createStore(rootReducer);

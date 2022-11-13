4a26fe3f19f6c9d3e2341d01b8fc71dfb23ced05
Hakan Bilgi <hknblg34@gmail.com>
2022-11-13 20:37:18 +0300

    store.jsx

--- clarusway-full-stack-tr-12-22/react/class-notes/rc22_redux-project/redux-project-inClass/src/app/store.jsx
+++ clarusway-full-stack-tr-12-22/react/class-notes/rc22_redux-project/redux-project-inClass/src/app/store.jsx
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import newsReducer from "../features/newsSlice";
 
+
 const store = configureStore({
   reducer: {
     auth: authReducer,

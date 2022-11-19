25c4eba38508ddad893f2c771b84b33aa6c5b7b1
Hakan Bilgi <hknblg34@gmail.com>
2022-11-19 23:10:03 +0300

    devToll

--- clarusway-full-stack-tr-12-22/react/class-notes/rc24_formik-yup/formik-yup-inClass/src/app/store.jsx
+++ clarusway-full-stack-tr-12-22/react/class-notes/rc24_formik-yup/formik-yup-inClass/src/app/store.jsx
@@ -3,10 +3,11 @@ import authReducer from "../features/authSlice";
 
 
 const store = configureStore({
-  
+
   reducer: {
     auth: authReducer,
   },
   devTools: process.env.NODE_ENV !== "production",
+  
 });
 export default store;

9fb262f81ae53ff761f892331c15f765e6f9047e
Hakan Bilgi <hknblg34@gmail.com>
2022-11-22 21:31:13 +0300

    custom hook

--- clarusway-full-stack-tr-12-22/react/class-notes/rc25_stock-app/stock-app/src/pages/Firms.jsx
+++ clarusway-full-stack-tr-12-22/react/class-notes/rc25_stock-app/stock-app/src/pages/Firms.jsx
@@ -1,28 +1,32 @@
 import { useEffect } from "react";
-import axios from "axios";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
+import useStockCalls from "../hooks/useStockCalls";
+// import axios from "axios";
+
+// import { useDispatch, useSelector } from "react-redux";
+// import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
 
 const Firms = () => {
-  const dispatch = useDispatch();
+  const { getFirms } = useStockCalls();
 
-  const { token } = useSelector((state) => state.auth);
-  const BASE_URL = "https://10001.fullstack.clarusway.com/";
+  // const dispatch = useDispatch();
 
-  const getFirms = async () => {
-    const url = "firms";
-    dispatch(fetchStart());
-    try {
-      const { data } = await axios.get(`${BASE_URL}stock/firms/`, {
-        headers: { Authorization: `Token ${token}` },
-      });
-      console.log(data);
-      dispatch(getSuccess({ data, url }));
-    } catch (error) {
-      dispatch(fetchFail());
-      console.log(error);
-    }
-  };
+  // const { token } = useSelector((state) => state.auth);
+  // const BASE_URL = "https://10001.fullstack.clarusway.com/";
+
+  // const getFirms = async () => {
+  //   const url = "firms";
+  //   dispatch(fetchStart());
+  //   try {
+  //     const { data } = await axios.get(`${BASE_URL}stock/firms/`, {
+  //       headers: { Authorization: `Token ${token}` },
+  //     });
+  //     console.log(data);
+  //     dispatch(getSuccess({ data, url }));
+  //   } catch (error) {
+  //     dispatch(fetchFail());
+  //     console.log(error);
+  //   }
+  // };
 
   useEffect(() => {
     getFirms();

d7d4b4f335295878d24c65d0b85fd5ec6981d67f
Hakan Bilgi <hknblg34@gmail.com>
2022-12-15 20:32:09 +0300

    defining class

--- clarusway-full-stack-tr-12-22/django/class-notes/S9_Python_OOP_1/oop1.py
+++ clarusway-full-stack-tr-12-22/django/class-notes/S9_Python_OOP_1/oop1.py
@@ -1,31 +1,81 @@
 import os
 os.system('cls' if os.name == 'nt' else 'clear')
+print("--------------------------------------")
 
-print("--------------------------------------------")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-print("------------------------------------------------")
-
+#! Topics to be Covered:
+#* Everything in Python is class
+#* Defining class
+#* Defining class attributes
+#* Difference between class attributes and instance attributes
+#* SELF keyword
+#* Defining methods
+#* Class Methods vs. Static Methods and Instance Methods
+#* Special methods (init, str)
+#* 4 pillars of OOP:
+    #? Encapsulation
+    #? Abstraction
+    #? Inheritance
+        #? Multiple inheritance
+    #? Polymorphism
+        #? Overriding methods
+#* Inner class
+#* Overloading an operator (optional)
+#* Abstract base class
 
 
+#! What is OOP?
+""" # Object Oriented programming (OOP) is a programming paradigm that relies on the concept of classes and objects.
+# It is used to structure a software program into simple, reusable pieces of code blueprints (usually called classes), which are used to create individual instances of objects.
+# significantly reduces code repetition by classifying similar structures (dont repeat yourself)
+# Easier to debug, classes often contain all applicable information to them
+# Secure, protects information through encapsulation """
+
+# nesne tabanlı programlamanın faydaları nelerdir?
+# --->Nesne tabanlı programlama sayesinde programlar daha anlaşılır, düzenli ve bakımı kolay hale gelir. OOP sayesinde, kod tekrar kullanımı ve modüler hale gelir. Bu da programların geliştirilmesini ve bakımını kolaylaştırır. Ayrıca, OOP sayesinde kod paylaşımı ve işbirliği daha kolay hale gelir.
+
+#! Everything in Python is class
+""" # Python >generally class based  vs.  javascript >generally function based
+def print_types(data):
+    for i in data:
+        print(i, type(i))
+
+test = [122, "victor", [1, 2, 3], (1,2,3), {1,2,3}, True, lambda x:x]
+
+print_types(test) """
+
+#! defining class:
+
+# "class" keyword for defining
+
+class Person:
+    pass
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("--------------------------------------")

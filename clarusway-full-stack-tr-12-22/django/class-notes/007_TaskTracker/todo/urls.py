9bf1aed1f262046105bbfec8ea96dfdb99107a92
Hakan Bilgi <hknblg34@gmail.com>
2022-12-27 22:47:12 +0300

    cbv

--- clarusway-full-stack-tr-12-22/django/class-notes/007_TaskTracker/todo/urls.py
+++ clarusway-full-stack-tr-12-22/django/class-notes/007_TaskTracker/todo/urls.py
@@ -3,10 +3,15 @@ from .views import (
     todo_list_create,
     todo_home,
     todo_detail,
+    Todo,
+    TodoDetail
 )
 
 urlpatterns = [
     path('', todo_home),
-    path('list-create/', todo_list_create),
-    path('detail/', todo_detail),
+    # path('list-create/', todo_list_create),
+    # path('detail/<int:id>', todo_detail),
+    path('list-create/', Todo.as_view()),
+    path('detail/<int:id>', TodoDetail.as_view()),
+
 ]

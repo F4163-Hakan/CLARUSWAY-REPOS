73f2daefa9f9d7610a1503ac44e4e43e09edb082
Hakan Bilgi <hknblg34@gmail.com>
2023-01-02 22:30:44 +0300

    serializer

--- clarusway-full-stack-tr-12-22/django/class-notes/010_BlogApp/blog/views.py
+++ clarusway-full-stack-tr-12-22/django/class-notes/010_BlogApp/blog/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from .models import Category
+
+from .models import Category, Blog
+from .serializers import CategorySerializer, BlogSerializer
 
 
-class CategoryView(ModelViewSet): #  crud
+class CategoryView(ModelViewSet):  # crud
     queryset = Category.objects.all()
-    serializer_class = None
+    serializer_class = CategorySerializer
+    filterset_fields = ['name']
+
+
+class BlogView(ModelViewSet):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+    filterset_fields = ['category']

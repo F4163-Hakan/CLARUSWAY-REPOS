55436b6e912a33bb361401481ca3fd050f151fda
Hakan Bilgi <hknblg34@gmail.com>
2023-02-01 20:15:26 +0300

    space

--- clarusway-full-stack-tr-12-22/django/class-notes/019_RentACar_App/car/permissions.py
+++ clarusway-full-stack-tr-12-22/django/class-notes/019_RentACar_App/car/permissions.py
@@ -2,7 +2,6 @@ from rest_framework import permissions
 
 
 class IsStaffOrReadOnly(permissions.IsAdminUser):
-    
 
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:

3298e2ad1b8dc894093472fe2d57d2d2079fbe16
Hakan Bilgi <hknblg34@gmail.com>
2023-02-06 22:42:44 +0300

    rev

--- clarusway-full-stack-tr-12-22/django/class-notes/020_StockApp/account/views.py
+++ clarusway-full-stack-tr-12-22/django/class-notes/020_StockApp/account/views.py
@@ -6,9 +6,6 @@ from rest_framework import status
 
 from .serializers import RegisterSerializer
 
-
-
-
 class RegisterView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer

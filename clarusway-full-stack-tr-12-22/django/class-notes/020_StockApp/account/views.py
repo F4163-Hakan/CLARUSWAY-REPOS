ff8f5b4976a10ce4f528e52fea8d73243d1675a7
Hakan Bilgi <hknblg34@gmail.com>
2023-02-07 21:49:06 +0300

    views

--- clarusway-full-stack-tr-12-22/django/class-notes/020_StockApp/account/views.py
+++ clarusway-full-stack-tr-12-22/django/class-notes/020_StockApp/account/views.py
@@ -3,7 +3,6 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework import status
-
 from .serializers import RegisterSerializer
 
 class RegisterView(CreateAPIView):

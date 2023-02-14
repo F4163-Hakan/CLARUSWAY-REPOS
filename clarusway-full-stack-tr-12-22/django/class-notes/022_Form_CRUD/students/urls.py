f255c2eb26f86de4740af9be068c6b8a3e3293fe
Hakan Bilgi <hknblg34@gmail.com>
2023-02-14 22:12:33 +0300

    forms django

---
+++ clarusway-full-stack-tr-12-22/django/class-notes/022_Form_CRUD/students/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import (
+    home,
+    student_list,
+    student_add,
+    student_update,
+)
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('list/', student_list, name='student_list'),
+    path('add/', student_add, name='student_add'),
+    path('update/<int:id>', student_update, name='student_update'),
+]
---
+++ clarusway-full-stack-tr-12-22/django/class-notes/022_Form_CRUD/students/views.py
@@ -0,0 +1,73 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+
+from .models import Student
+from .forms import StudentForm
+
+
+def home(request):
+
+    context = {
+        'title': 'clarusway',
+        'desc': 'This is a description',
+        'number': 1111,
+        'list1': ['a', 1, 'b', ['c', 2]],
+        'dict1': {
+            'key1': 'value1',
+            'key2': 'value2'
+        },
+        'dict_list': [
+            {'name': 'zed', 'age': 25},
+            {'name': 'amy', 'age': 21},
+            {'name': 'joe', 'age': 50},
+        ]
+
+    }
+
+    return render(request, 'students/home.html', context)
+
+    # return HttpResponse('<p> Hello </p>')
+
+
+'''
+{{ variables }}
+{% command %}
+| ---> filter
+'''
+
+
+def student_list(request):
+    students = Student.objects.all()
+    context = {
+        'students': students
+    }
+
+    return render(request, 'students/student_list.html', context)
+
+
+def student_add(request):
+    form = StudentForm()
+    if request.method == 'POST':
+        # print('POST :', request.POST)
+        # print('fıles :', request.FILES)
+        form = StudentForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+            return redirect('student_list')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'students/student_add.html', context)
+
+
+def student_update(request, id):
+    student = get_object_or_404(Student, id=id)
+    form = StudentForm(instance=student)
+
+    context = {
+        'form': form
+    }
+    return render(request, 'students/student_update.html', context)

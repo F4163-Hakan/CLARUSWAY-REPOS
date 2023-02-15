74bd1d8e2f350ab8dd167ac8be80c09ef9c31386
Hakan Bilgi <hknblg34@gmail.com>
2023-02-15 23:04:13 +0300

    class based todo

--- clarusway-full-stack-tr-12-22/django/class-notes/023_ToDoApp/todo/urls.py
+++ clarusway-full-stack-tr-12-22/django/class-notes/023_ToDoApp/todo/urls.py
@@ -4,5 +4,8 @@ from .views import *
 urlpatterns = [
     path('',todo_list,name='todo_list' ),
     path('add/',todo_add,name='todo_add' ),
+    path('update/<int:pk>',todo_update,name='todo_update' ),
+    path('delete/<int:pk>',todo_delete,name='todo_delete' ),
+
 
 ]

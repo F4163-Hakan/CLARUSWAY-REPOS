112b06e66ab0230fe592c68381d0d9b68ee8b480
Hakan Bilgi <hknblg34@gmail.com>
2023-02-16 21:52:19 +0300

    login add

--- clarusway-full-stack-tr-12-22/django/class-notes/024_PizzaApp/users/views.py
+++ clarusway-full-stack-tr-12-22/django/class-notes/024_PizzaApp/users/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
-
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import UserForm
 
 def register(request):
-    return render(request,'users/register.html')
+    # form = UserCreationForm()
+    form = UserForm()
+    
+    if request.method == 'POST':
+        # form = UserCreationForm(request.POST)
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+            
+    
+    context = {
+        "form": form
+    }
+    return render(request,'users/register.html', context)

ad6ff63d4d3ac4d212da99f687a38dadd76493ac
Hakan Bilgi <hknblg34@gmail.com>
2023-03-22 22:47:23 +0300

    Object Literals

--- clarusway-full-stack-tr-12-22/javascript/class-notes/js21_oop1/1-objectLiteral.js
+++ clarusway-full-stack-tr-12-22/javascript/class-notes/js21_oop1/1-objectLiteral.js
@@ -17,6 +17,7 @@ const book1 = {
 console.log(book1);
 console.log(book1.getSummary());
 console.log(book1.hasOwnProperty("author")); //? true
+
 //! Object nesnesinden miras yoluyla geldi.
 
 const book2 = {
@@ -32,5 +33,7 @@ console.log(book2);
 console.log(book2.getSummary());
 
 //? Object literals yontemi ile fazla sayida yeni objeler olusturmak oldukca zahmetlidir.
+
 //? Ayrica programcilik yaklasimi acisindan da cok tekrar icerdigi icin uygun degildir (DRY - Dont Repeat Yourself)
+
 //? Cozum: Object Oriented Programming (ES5 and ES6)

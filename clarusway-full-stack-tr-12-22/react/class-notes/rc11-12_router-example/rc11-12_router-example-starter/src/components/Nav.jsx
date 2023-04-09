3c0c0032464c500061c82700419f08efae67a1cf
Hakan Bilgi <hknblg34@gmail.com>
2023-04-09 23:15:35 +0300

    navbar

--- clarusway-full-stack-tr-12-22/react/class-notes/rc11-12_router-example/rc11-12_router-example-starter/src/components/Nav.jsx
+++ clarusway-full-stack-tr-12-22/react/class-notes/rc11-12_router-example/rc11-12_router-example-starter/src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import logo from "../img/logo.png";
 
 function Nav() {
+
   return (
     <nav className="navbar navbar-expand-md navbar-light">
       <div className="container-fluid">
@@ -17,6 +18,7 @@ function Nav() {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 me-3 mb-lg-0">
+            
             <li className="nav-item">
               <a href="/" className="nav-link active" aria-current="page">
                 Home

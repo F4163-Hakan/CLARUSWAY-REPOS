97813114b15d3fdda31a464b04d9adbc6e6e6df6
Hakan Bilgi <hknblg34@gmail.com>
2023-04-11 21:25:30 +0300

    recipe card

--- clarusway-full-stack-tr-12-22/react/class-notes/rc14-15_recipe-app/recipe-app-solution/src/components/cards/Cards.jsx
+++ clarusway-full-stack-tr-12-22/react/class-notes/rc14-15_recipe-app/recipe-app-solution/src/components/cards/Cards.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Button, Card, Header, Image, MainContainer } from "./Cards.style";
 import defaultImage from "../../assets/default-image.jpg"
 import {useNavigate} from "react-router-dom"
+
+
 const Cards = ({ recipes }) => {
   const navigate = useNavigate();
   return (

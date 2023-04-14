0823abd45365f65239d1c67d0cb82b3c4e3052d2
Hakan Bilgi <hknblg34@gmail.com>
2023-04-14 21:50:28 +0300

    context

--- clarusway-full-stack-tr-12-22/react/class-notes/rc16_context-api/context-example1/context-example1-inClass/src/components/Footer.jsx
+++ clarusway-full-stack-tr-12-22/react/class-notes/rc16_context-api/context-example1/context-example1-inClass/src/components/Footer.jsx
@@ -2,6 +2,7 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 
 function Footer() {
+  
   return (
     <Container className="text-center" style={{ marginTop: "10rem" }}>
       <Navbar fixed="bottom" className="justify-content-center bg-dark p-4">

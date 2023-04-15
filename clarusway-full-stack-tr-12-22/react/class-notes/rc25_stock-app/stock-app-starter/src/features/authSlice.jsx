12777d6c3274caa41bfb78585e1b30aec34a4c7f
Hakan Bilgi <hknblg34@gmail.com>
2023-04-15 23:06:05 +0300

    stokApp

--- clarusway-full-stack-tr-12-22/react/class-notes/rc25_stock-app/stock-app-starter/src/features/authSlice.jsx
+++ clarusway-full-stack-tr-12-22/react/class-notes/rc25_stock-app/stock-app-starter/src/features/authSlice.jsx
@@ -10,28 +10,33 @@ const authSlice = createSlice({
     isAdmin: false,
     token: null,
   },
+
   reducers: {
     fetchStart: (state) => {
       state.loading = true;
       state.error = false;
     },
+
     loginSuccess: (state, { payload }) => {
       state.loading = false;
       state.currentUser = payload?.user?.username;
       state.isAdmin = payload?.user?.is_superuser;
       state.token = payload?.key;
     },
+
     logoutSuccess: (state) => {
       state.loading = false;
       state.currentUser = null;
       state.token = null;
     },
+
     registerSuccess: (state, { payload }) => {
       state.loading = false;
       state.currentUser = payload?.username;
       state.token = payload?.token;
       state.error = false;
     },
+    
     fetchFail: (state) => {
       state.loading = false;
       state.error = true;
